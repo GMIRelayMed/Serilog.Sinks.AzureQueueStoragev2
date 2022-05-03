e4f78893551f1ca48dff041b6eff9dd4695fba87
James H <33937376+jimmytricks@users.noreply.github.com>
2022-05-03 16:16:08 +0100

    add text formatter to logger configuration

--- src/Serilog.Sinks.AzureQueueStoragev2/AzureQueueStorageSink.cs
+++ src/Serilog.Sinks.AzureQueueStoragev2/AzureQueueStorageSink.cs
@@ -2,6 +2,8 @@
 using Newtonsoft.Json;
 using Serilog.Core;
 using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Json;
 
 namespace Serilog.Sinks.AzureQueueStoragev2
 {
@@ -9,11 +11,13 @@ namespace Serilog.Sinks.AzureQueueStoragev2
     {
         private readonly string ConnectionString;
         private readonly string QueueName;
+        private readonly ITextFormatter textFormatter;
 
         public AzureQueueStorageSink(string ConnectionString, string QueueName)
         {
             this.ConnectionString = ConnectionString;
             this.QueueName = QueueName;
+            this.textFormatter = new JsonFormatter("", false, null);
         }
 
         public void Emit(LogEvent logEvent)
